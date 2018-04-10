8c5110426c5544d355ac63a0d55c4c6d700c1206
malkyatmuk1 <tanya.naidenova@abv.bg>
2018-04-10 17:31:42 +0300

    smaller drawableleft

--- SmartFix/app/src/main/java/com/example/smartfix/smartfix/SecondForm.java
+++ SmartFix/app/src/main/java/com/example/smartfix/smartfix/SecondForm.java
@@ -1,9 +1,20 @@
 package com.example.smartfix.smartfix;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -13,45 +24,99 @@ import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-public class SecondForm extends Activity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-    Boolean isAndroid=false;
+public class SecondForm extends AppCompatActivity {
+
+    Boolean isAndroid = false;
     String text;
-    EditText problem,model;
+    EditText problem, model;
     RadioGroup radioGroup;
-    RadioButton radioButtonAndroid,radioButtonIos;
+    RadioButton radioButtonAndroid, radioButtonIos;
+    LocationManager locationManager;
+
+    Double lat, lng;
+    String cityName, countryName, stateName;
+   Geocoder geocoder;
+   private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_form);
-        Button next= (Button) findViewById(R.id.button);
-         radioButtonAndroid= (RadioButton) findViewById(R.id.radio_android);
-         radioButtonIos = (RadioButton) findViewById(R.id.radio_ios);
-         radioGroup=(RadioGroup) findViewById(R.id.radio);
-         problem= (EditText) findViewById(R.id.problem);
-         model= (EditText) findViewById(R.id.model);
-
+        Button next = (Button) findViewById(R.id.button);
+        radioButtonAndroid = (RadioButton) findViewById(R.id.radio_android);
+        radioButtonIos = (RadioButton) findViewById(R.id.radio_ios);
+        radioGroup = (RadioGroup) findViewById(R.id.radio);
+        problem = (EditText) findViewById(R.id.problem);
+        model = (EditText) findViewById(R.id.model);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
         next.setOnClickListener(emailListener);
         radioGroup.setOnCheckedChangeListener(radioListener);
-        problem.setOnTouchListener(new View.OnTouchListener() {
 
-            public boolean onTouch(View view, MotionEvent event) {
 
-                if (view.getId() ==R.id.problem) {
-                    view.getParent().requestDisallowInterceptTouchEvent(true);
-                    switch (event.getAction()&MotionEvent.ACTION_MASK){
-                        case MotionEvent.ACTION_UP:
-                            view.getParent().requestDisallowInterceptTouchEvent(false);
-                            break;
-                    }
-                }
-                return false;
+        locationListener= new LocationListener() {
+            @Override
+            public void onLocationChanged( Location location) {
+                lat = location.getLatitude();
+                lng = location.getLongitude();
             }
-        });
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, locationListener);
+        getLocation(this,lat,lng);
 
     }
 
+
+
+    public void getLocation(Context cont,Double lat,Double lng) {
+
+
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            addresses = geocoder.getFromLocation(lat, lng, 1);
+            if(addresses.size()>0)
+            {
+                cityName = addresses.get(0).getAddressLine(0);
+                stateName = addresses.get(0).getAddressLine(1);
+                //Toast.makeText(getApplicationContext(),stateName , 1).show();
+                countryName = addresses.get(0).getAddressLine(2);
+            }
+        }catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
     View.OnClickListener emailListener=new View.OnClickListener() {
 
         public void onClick(View view) {
@@ -59,7 +124,14 @@ public class SecondForm extends Activity {
             if(isAndroid) text = "Android - " + model.getText().toString()+"\n" ;
             else text="IOS  - "+ model.getText().toString() + "\n";
             text= text+ problem.getText().toString()+ "\n";
+            text= text+ cityName +" "+ countryName + " "+ stateName + "\n";
             String address = "tanya.naidenova@abv.bg";
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+
+
+
             composeEmail(address, Form.getName(),text);
 
 
@@ -102,4 +174,5 @@ public class SecondForm extends Activity {
                     break;
         }
     }
-}
+
+ }
